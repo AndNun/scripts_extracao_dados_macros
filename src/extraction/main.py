7f62c448ab0e3abcd5282945a42b9f33f18dbaa8
AndNun <andsonunes@gmail.com>
2025-04-18 18:06:26 -0300

    update

--- src/extraction/main.py
+++ src/extraction/main.py
@@ -18,12 +18,19 @@ df_ipca_proj.head()
 
 df_ipca_proj['dt_inclusao'] = pd.to_datetime('today')
 
+# Carregando dados reais ipca mensal
+df_ipca_real = utils.consulta_bcb(433)
+
+df_ipca_real.head()
+
+df_ipca_real['dt_inclusao'] = pd.to_datetime('today')
 # %%
 # Conectando ao banco SQLite (ou criando se não existir)
 conexao = sqlite3.connect('data/macroeconomico.db')
 
-# Salvando o dataframe no banco de dados do SQLite
+# Salvando o dataframe no banco de dados so SQLite
+df_ipca_real.to_sql('ipca_mensal_real', conexao, if_exists = 'append', index = False)
 df_ipca_proj.to_sql('ipca_mensal_projecao', conexao, if_exists = 'append', index = False)
 
+# Fechando conexão
 conexao.close()
-
